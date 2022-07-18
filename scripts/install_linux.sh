638969ac49bee12eb61fad27303a56298d263f69
Sai Prasad TN <saiprasadt@quinnox.com>
2022-07-19 02:29:55 +0530

    change in path directory

--- scripts/install_linux.sh
+++ scripts/install_linux.sh
@@ -5,4 +5,4 @@ release_version=$1
 wget https://github.com/QQyrus/node-cli/releases/download/$release_version/qyrus-cli-linux
 chmod 755 qyrus-cli-linux
 echo "Copying the binary to /bin/"
-mv qyrus-cli-macos /bin/qyrus-cli && echo $? || echo $?
+mv qyrus-cli-linux /usr/local/bin/qyrus-cli && echo $? || echo $?
