e4b08a9c2dbb0e972a2eede022654eb4187dd2dc
Sai Prasad TN <saiprasadt@quinnox.com>
2022-07-19 02:32:07 +0530

    changing the path to /tmp

--- scripts/install_linux.sh
+++ scripts/install_linux.sh
@@ -4,5 +4,5 @@ release_version=$1
 
 wget https://github.com/QQyrus/node-cli/releases/download/$release_version/qyrus-cli-linux
 chmod 755 qyrus-cli-linux
-echo "Copying the binary to /bin/"
-mv qyrus-cli-linux /usr/local/bin/qyrus-cli && echo $? || echo $?
+echo "Copying the binary to /tmp/"
+mv qyrus-cli-linux /tmp/qyrus-cli && echo $? || echo $?
